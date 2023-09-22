2a4da92daa0e6fe372845f43797759718afd485a
Jebbril <orakib@student.1337.ma>
2023-09-22 17:46:01 +0100

    done

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 20:08:41 by orakib            #+#    #+#             */
-/*   Updated: 2023/09/22 17:39:20 by orakib           ###   ########.fr       */
+/*   Updated: 2023/09/22 17:45:48 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,51 @@
 
 int	main(void) {
 	{
-		Intern someRandomIntern;
-		AForm* rrf;
-		rrf = someRandomIntern.makeForm("robotomy request", "Bender");
-		delete rrf;
+		try {
+			Intern someRandomIntern;
+			AForm* rrf;
+			rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+			delete rrf;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 	{
-		Intern someRandomIntern;
-		AForm* scf;
-		scf = someRandomIntern.makeForm("shrubbery creation", "home");
-		delete scf;
+		try {
+			Intern someRandomIntern;
+			AForm* scf;
+			scf = someRandomIntern.makeForm("shrubbery creation", "home");
+			delete scf;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 	{
-		Intern someRandomIntern;
-		AForm* ppf;
-		ppf = someRandomIntern.makeForm("presedential pardon", "jeff");
-		delete ppf;
+		try {
+			Intern someRandomIntern;
+			AForm* ppf;
+			ppf = someRandomIntern.makeForm("presedential pardon", "jeff");
+			delete ppf;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 	{
-		Intern someRandomIntern;
-		AForm* ppf;
-		ppf = someRandomIntern.makeForm("something", "jeff");
-		delete ppf;
+		try {
+			Intern someRandomIntern;
+			AForm* ppf;
+			ppf = someRandomIntern.makeForm("something", "jeff");
+			delete ppf;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
 }
